0265c09be5b2b461f8cd37cf185a20736a2b672f
kathir <kkathir42@gmail.com>
2018-02-16 01:13:58 +0530

    second commit

---
+++ biography.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>JMRB</title>
+<!-- Stylesheets -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/revolution-slider.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet">
+
+<!--Favicon-->
+<link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
+<!-- Responsive -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<link href="css/responsive.css" rel="stylesheet">
+
+</head>
+
+<body>
+<div class="page-wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader"></div> 
+	
+    <?php include('header.php');?>
+   
+  <!--Page Title-->
+    <section class="page-title" style="background-image: url(images/background/4.jpg);">
+        <div class="auto-container">
+            <h1>Biography</h1>
+            <ul class="bread-crumb">
+                <li><a href="index.php">Home</a></li>
+                <li class="active">Biography</li>
+            </ul>
+        </div>
+    </section>
+    <!--End Page Title-->
+
+
+
+    <!--Two Default Section-->
+    <section class="about-section">
+        <div class="auto-container">
+            
+            <div class="row clearfix">
+
+                <div class="about-column column col-md-7 col-sm-12 col-xs-12">
+                    <div class="inner">
+                        
+                        <h3 class="title">How you can save time &amp; money in your Business</h3>
+                        <div class="text">
+                            We’re a team of accredited plumbing contractors with 20 years expertise in the trade. We are well equipped to handle the complex plumbing issues and have an excellent comprehension of your plumbing problems brings great trouble, or worse, serious damages to your premises.
+                        </div>
+                        <div class="row clearfix">
+                            <!--Text Column-->
+                            <div class="text-column col-md-6 col-sm-6 col-xs-12">
+                                <h3>Who We Are?</h3>
+                                <div class="text">Lorem ipsum dolor sit amet vix antulm natum labitur eleifend, mel amet laorit menandri quality cheker. </div>
+                                <div class="btn-box"><a href="about.html" class="theme-btn btn-style-one">Learn More</a></div>
+                            </div>
+                            <!--Text Column-->
+                            <div class="text-column col-md-6 col-sm-6 col-xs-12">
+                                <h3>Our Mission</h3>
+                                <div class="text">Lorem ipsum dolor sit amet vix antulm natum labitur eleifend, mel amet laorit menandri quality cheker. </div>
+                                <div class="signature"><img src="images/resource/signature.png" alt=""></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--video Column-->
+                <div class="video-column col-md-5 col-sm-12 col-xs-12">
+                    <!--video-box-->
+                    <div class="video-image-box">
+                        <figure class="image">
+                            <img src="images/gallery/10.jpg" alt="">
+                            <a href="https://www.youtube.com/watch?v=nfP5N9Yc72A&amp;t=28s" class="overlay-link lightbox-image video-fancybox"><span class="icon flaticon-arrow"></span></a>
+                        </figure>                        
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    </section>
+    <!--End Two Default Section-->
+
+
+
+    <!--Subscribe Style One-->
+    <section class="subscribe-style-one">
+        <div class="auto-container">
+            <div class="row clearfix">
+                <div class="col-md-7 col-sm-12">
+                    <h2>Sign Up for Newsletter</h2>
+                    <div class="text">We’ll be with you on every walk of life on how to identify new opportunities.</div>
+                </div>
+                <div class="col-md-5 col-sm-12">
+                    <form method="post" action="http://html.tonatheme.com/2017/consultation/contact.html">
+                        <div class="form-group">
+                            <input type="email" name="email" value="" placeholder="Enter your Email" required>
+                            <button type="submit" class="theme-btn btn-style-one">SUBMIT</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--End Subscribe Style One-->
+
+	
+    <?php include('footer.php');  ?>
+
+</div>
+<!--End pagewrapper-->
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target=".main-header"><span class="fa fa-long-arrow-up"></span></div>
+
+
+<!--Search Popup-->
+<div id="search-popup" class="search-popup">
+	<div class="close-search theme-btn"><span class="fa fa-close"></span></div>
+	<div class="popup-inner">
+    
+    	<div class="search-form">
+        	<form method="post" action="http://html.tonatheme.com/2017/consultation/index.html">
+            	<div class="form-group">
+                	<fieldset>
+                        <input type="search" class="form-control" name="search-input" value="" placeholder="Search Here" required >
+                        <input type="submit" value="Search" class="theme-btn">
+                    </fieldset>
+                </div>
+            </form>
+            
+            <br>
+            <h3>Recent Search Keywords</h3>
+            <ul class="recent-searches">
+                <li><a href="#">Business</a></li>
+                <li><a href="#">Web Development</a></li>
+                <li><a href="#">SEO</a></li>
+                <li><a href="#">Logistics</a></li>
+                <li><a href="#">Freedom</a></li>
+            </ul>
+        
+        </div>
+        
+    </div>
+</div>
+<!--End Search Popup-->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/revolution.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>
+<script src="js/owl.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/parallax.js"></script>
+<script src="js/script.js"></script>
+</body>
+
+</html>
